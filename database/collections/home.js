b18b3cf669091086c23c67c49b07a29e550a283b
greyss123 <greyss123@gmail.com>
2019-11-28 22:45:51 -0400

    home1

---
+++ database/collections/home.js
@@ -0,0 +1,24 @@
+const mongoose = require("../connect");
+var mon = require('mongoose');
+var Schema = mon.Schema;
+var homeSchema = new Schema({
+  clientehome: {
+    type: Schema.Types.ObjectId,
+    ref: "Clientehome"},
+  propietario : String,
+  descripcion : String,
+  precio : Number,
+  contacto: String,
+
+  zona : String,
+  calle : String,
+  telefono : Number,
+  lat : String,
+  lon : String,
+  Fecha_Registro: {
+    type: Date, default: Date.now
+  },
+  fotolugar : Array
+});
+var home = mongoose.model("Home", homeSchema);
+module.exports = home;
