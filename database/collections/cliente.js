4cf29b5b6ded07f68d90b325e684c992813d2559
greyss123 <greyss123@gmail.com>
2019-12-16 02:06:22 -0400

    clientes2

--- database/collections/cliente.js
+++ database/collections/cliente.js
@@ -2,14 +2,3 @@ const mongoose = require("../connect");
 var mon = require('mongoose');
 var Schema = mon.Schema;
 var clienteSchema = new Schema({
-  nombre : String,
-  ci : String,
-  
-  Fecha_Registro: {
-      type: Date,
-      default: Date.now()
-  },
-  tipo : String
-});
-var cliente = mongoose.model("Cliente", clienteSchema);
-module.exports = cliente;
