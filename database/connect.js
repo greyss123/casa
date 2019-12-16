4dd6484c461ba207c4273e45ffa0648d8a87518c
greyss123 <greyss123@gmail.com>
2019-12-16 02:04:46 -0400

    casa1

--- database/connect.js
+++ database/connect.js
@@ -1,10 +1,2 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27020/casa", {
-  useNewUrlParser: true
-}).then(()=>{
-  console.log('connexion a mongodb existosa');
-}).catch(err => {
-  console.log('error en la connexion', err);
-});
-
-module.exports = mongoose;
